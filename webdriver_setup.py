05c5ec0bbcfded2461b8f4fce6f9007967d3aa3a
mas <mas@localhost.localdomain>
2025-04-15 00:26:05 +0530

    Fix: rename setup_driver to create_chrome_driver

--- webdriver_setup.py
+++ webdriver_setup.py
@@ -1,9 +1,8 @@
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
-def setup_driver():
+def create_chrome_driver():
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
@@ -13,3 +12,4 @@ def setup_driver():
     service = Service("/usr/bin/chromedriver")
     driver = webdriver.Chrome(service=service, options=chrome_options)
     return driver
+
